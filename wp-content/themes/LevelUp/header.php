0d56f6bc44520ec58742d5667e36def28fdb60d0
delux <deluxart3@gmail.com>
2019-07-16 15:30:50 +0300

    reset

--- wp-content/themes/LevelUp/header.php
+++ wp-content/themes/LevelUp/header.php
@@ -31,7 +31,7 @@
 
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=cyrillic,cyrillic-ext,latin-ext,vietnamese" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/dist/styles-all.min.css?<?php echo date(get_option('date_format')); ?>3">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/dist/styles-all.min.css?123<?php echo date(get_option('date_format')); ?>3">
 
     <?php wp_head(); ?>
 
