d013d2ceea942a2bb5f73666d58dfba3d999cfe0
delux <deluxart3@gmail.com>
2019-08-08 17:20:58 +0300

    New quote block

---
+++ wp-content/themes/LevelUp/blocks/block-quote-two.php
@@ -0,0 +1,8 @@
+<div class="block-quote-two" style="margin-bottom: <?php block_field( 'margin-bottom' ); ?>px; border-color: <?php block_field( 'border-color' ); ?>; font-size: <?php block_field( 'font-size' ); ?>px">
+	<?php block_field( 'content' ); ?>
+</div>
+<?php
+if ( block_value( ' font-italic' ) ) {
+	echo "<style>.block-quote-two { font-style: italic !important; }</style>";
+}
+?>
