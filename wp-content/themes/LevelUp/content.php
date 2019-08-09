dd3f03f6cdc6cbbe4813d5136d8fe3acc311f750
delux <deluxart3@gmail.com>
2019-08-09 14:50:44 +0300

    fix

--- wp-content/themes/LevelUp/content.php
+++ wp-content/themes/LevelUp/content.php
@@ -11,12 +11,8 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php
-		// Post thumbnail.
-		// LevelUp_post_thumbnail();
-	?>
 
-<?php the_post_thumbnail(array()); ?>
+
 
 	<header class="entry-header">
 		<?php
@@ -26,27 +22,62 @@
 				the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
 			endif;
 		?>
+	<ul class="tag">
+ 		<li class="date"><span><i class="fa fa-calendar" aria-hidden="true"></i><?php the_date(); ?></span></li>
+ 		<li class="cat"><span><i class="fa fa-tags" aria-hidden="true"></i><?php the_tags(); ?></span></li>
+ 	</ul>
+
+
+
 	</header><!-- .entry-header -->
 
+
+
+
+<div class="post-image">
+	<?php the_post_thumbnail(array()); ?>
+
+
+<?php
+    $category = get_the_category();
+    $the_category_id = $category[0]->cat_ID;
+
+    if(function_exists('rl_color')){
+        $rl_category_color = rl_color($the_category_id);
+    }
+?>
+
+	<span class="category"><?php
+$categories = get_the_category(); 
+if($categories[0]){
+	echo '<a href="' . get_category_link($categories[0]->term_id ) . '" style="background: ' . $rl_category_color . '">'. $categories[0]->name . '</a>';
+}
+	?></span>
+</div>
+
 	<div class="entry-content">
+
+<div id="share-bar"></div>
+		
 		<?php
 			/* translators: %s: Name of current post */
 			the_content( sprintf(
-				__( 'Continue reading %s', 'LevelUp' ),
+				__( 'Подробнее %s', 'Smart' ),
 				the_title( '<span class="screen-reader-text">', '</span>', false )
 			) );
 
 			wp_link_pages( array(
-				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'LevelUp' ) . '</span>',
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'Smart' ) . '</span>',
 				'after'       => '</div>',
 				'link_before' => '<span>',
 				'link_after'  => '</span>',
-				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'LevelUp' ) . ' </span>%',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'Smart' ) . ' </span>%',
 				'separator'   => '<span class="screen-reader-text">, </span>',
 			) );
 		?>
 	</div><!-- .entry-content -->
-
+	<!-- <div class="fb-btn"><?php echo do_shortcode('[fb_button]'); ?></div> -->
+<?php if (function_exists('dw_reactions')) { dw_reactions(); } ?>
 	<?php
 		// Author bio.
 		if ( is_single() && get_the_author_meta( 'description' ) ) :
@@ -55,8 +86,24 @@
 	?>
 
 	<footer class="entry-footer">
-		<?php LevelUp_entry_meta(); ?>
-		<?php edit_post_link( __( 'Edit', 'LevelUp' ), '<span class="edit-link">', '</span>' ); ?>
+		<?php Smart_entry_meta(); ?>
+		<?php edit_post_link( __( 'Edit', 'Smart' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
 
 </article><!-- #post-## -->
+
+
+
+<div class="block-comments" style="display: none;">
+	<h2>Комментарии Facebook:</h2>
+	<div class="content">
+		<?php echo do_shortcode('[gs-fb-comments]'); ?>
+	</div>
+</div>
+
+<div class="block-tags" style="display: none;">
+	<h2>Облако тегов:</h2>
+	<div class="content">
+		<?php wp_tag_cloud('largest=15&smallest=14&unit=px&number=10'); ?>
+	</div>
+</div>
