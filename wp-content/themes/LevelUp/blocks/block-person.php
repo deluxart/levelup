3e72fd849d7750065c84fef928e56d7514c47b77
delux <deluxart3@gmail.com>
2019-08-12 11:31:34 +0300

    fix

--- wp-content/themes/LevelUp/blocks/block-person.php
+++ wp-content/themes/LevelUp/blocks/block-person.php
@@ -1,4 +1,10 @@
 <div class="block-person" style="margin-bottom: <?php block_field( 'margin-bottom' ); ?>px;">
     <div class="photo"><img src="<?php block_field( 'photo' ); ?>" alt="" /></div>
-	<div class="text"><?php block_field( 'content' ); ?></div>
+	<div class="text">
+        <div>
+            <h4><?php block_field( 'title' ); ?></h4>
+            <p class="position"><?php block_field( 'position' ); ?></p>
+            <?php block_field( 'content' ); ?>
+        </div>
+    </div>
 </div>
