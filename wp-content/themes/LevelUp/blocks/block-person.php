9cec201e711cd3514ee915b70b3224b0b933ca3d
delux <deluxart3@gmail.com>
2019-08-12 10:49:42 +0300

    new_block_person

---
+++ wp-content/themes/LevelUp/blocks/block-person.php
@@ -0,0 +1,4 @@
+<div class="block-person" style="margin-bottom: <?php block_field( 'margin-bottom' ); ?>px;">
+    <div class="photo"><img src="<?php block_field( 'photo' ); ?>" alt="" /></div>
+	<div class="text"><?php block_field( 'content' ); ?></div>
+</div>
