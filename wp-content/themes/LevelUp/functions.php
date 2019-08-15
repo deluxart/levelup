f4191267b096e9d597aef69cabc96e39757e04c4
DELUX <delux@339-3-1.training.workrocks.ua>
2019-08-15 10:23:46 +0300

    fa

--- wp-content/themes/LevelUp/functions.php
+++ wp-content/themes/LevelUp/functions.php
@@ -748,7 +748,7 @@ function replace_core_jquery_version() {
 add_action( 'wp_enqueue_scripts', 'replace_core_jquery_version' );
 
 function my_stylesheet1(){
-    wp_enqueue_style("style-admin",get_bloginfo('stylesheet_directory')."/assets/font-awesome.min.css");
+    wp_enqueue_style("style-admin",get_bloginfo('stylesheet_directory')."/assets/font-awesome2.min.css");
 }
 add_action('admin_head', 'my_stylesheet1');
 
