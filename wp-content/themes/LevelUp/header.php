59fb03d3199486cab92371f285cc961fbf97a70c
delux <deluxart3@gmail.com>
2019-08-28 17:37:15 +0300

    fix

--- wp-content/themes/LevelUp/header.php
+++ wp-content/themes/LevelUp/header.php
@@ -29,6 +29,10 @@
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/bootstrap/bootstrap.min.css?1">
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/font-awesome.min.css?1">
 
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/career-page.css?1">
+
+
+
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=cyrillic,cyrillic-ext,latin-ext,vietnamese" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/dist/styles-all.min.css?321<?php echo date(get_option('date_format')); ?>3">
