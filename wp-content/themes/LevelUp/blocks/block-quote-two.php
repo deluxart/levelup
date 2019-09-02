6fcc109a70f8e39526225352e6309718b2a09e34
delux <deluxart3@gmail.com>
2019-09-02 15:00:57 +0300

    fix

--- wp-content/themes/LevelUp/blocks/block-quote-two.php
+++ wp-content/themes/LevelUp/blocks/block-quote-two.php
@@ -1,4 +1,4 @@
-<div class="block-quote-two" style="margin-bottom: <?php block_field( 'margin-bottom' ); ?>px; border-color: <?php block_field( 'border-color' ); ?>; font-size: <?php block_field( 'font-size' ); ?>px">
+<div class="block-quote-two" style="margin-bottom: <?php block_field( 'margin-bottom' ); ?>px; border-color: <?php block_field( 'border-color' ); ?>; font-size: <?php block_field( 'font-size' ); ?>px;color: <?php block_field( 'color' ); ?>">
 	<?php block_field( 'content' ); ?>
 </div>
 <?php
