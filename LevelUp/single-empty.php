28d4f22648234566762274fd8a454203e1efa2b0
delux <deluxart3@gmail.com>
2019-09-27 15:19:32 +0300

    Add empty template

--- LevelUp/single-empty.php
+++ LevelUp/single-empty.php
@@ -6,6 +6,21 @@
 
 get_header(); ?>
 <main id="main" class="site-main" role="main">
-ashdasjdkasdjhhfhfhhfhhhf
+<?php
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Подробнее %s', 'LevelUp' ),
+				the_title( '<span class="screen-reader-text">', '</span>', false )
+			) );
+
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'LevelUp' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'LevelUp' ) . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
+			) );
+		?>
 </main>
 <?php get_footer(); ?>
