25b634f9fd4655054bb2485a2d6e192df6b67b37
delux <deluxart3@gmail.com>
2019-09-27 15:16:00 +0300

    fix

---
+++ LevelUp/single-empty.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: Для HTML-новостей
+ * Template Post Type: post
+ */
+
+get_header(); ?>
+<main id="main" class="site-main" role="main">
+ashdasjdkasdjhhfhfhhfhhhf
+</main>
+<?php get_footer(); ?>
