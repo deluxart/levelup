9b26bd112cdd7e352c0d8b1492f8445b72f254f9
delux <deluxart3@gmail.com>
2019-10-02 17:36:11 +0300

    fix

--- LevelUp/functions.php
+++ LevelUp/functions.php
@@ -897,7 +897,7 @@ echo '<div class="events_block">';
 	if ( have_posts() ) :
 	        while ( have_posts() ) : the_post();
 
-	            get_template_part( 'template-parts/content', get_post_format() );
+	            get_template_part( 'template-parts/widget', get_post_format() );
 
 	        endwhile;
 	    else :
---
+++ LevelUp/template-parts/widget.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+<div class="post-image">
+	<?php the_post_thumbnail('thumbnail'); ?>
+</div>
+
+	<header class="entry-header">
+		<?php
+			if ( is_single() ) :
+				the_title( '<h1>', '</h1>' );
+			else :
+				the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+			endif;
+		?>
+	<ul class="tag">
+         <li class="date"><span><i class="fa fa-calendar" aria-hidden="true"></i><?php the_date(); ?></span></li>
+
+
+ 	</ul>
+
+
+
+	</header><!-- .entry-header -->
+
+
+
+</article><!-- #post-## -->
