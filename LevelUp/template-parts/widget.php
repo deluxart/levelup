ebe671666fa623a977d5b7e4794679755ad8eca9
delux <deluxart3@gmail.com>
2019-10-02 17:57:58 +0300

    fix

--- LevelUp/template-parts/widget.php
+++ LevelUp/template-parts/widget.php
@@ -20,14 +20,11 @@
 			if ( is_single() ) :
 				the_title( '<h4>', '</h4>' );
 			else :
-				the_title( sprintf( '<h4 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h4>' );
+				the_title( sprintf( '<a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a>' );
 			endif;
-		?>
-<?php if ( get_field( 'date' ) ) { ?>
-	<?php the_field( 'date' ); ?>
-<?php } ?>
+        ?>
 <?php if ( get_field( 'time' ) ) { ?>
-	о <?php the_field( 'time' ); ?>
+	<div class="date-event"><i class="fa fa-clock-o" aria-hidden="true"></i> <?php the_field( 'date' ); ?> о <?php the_field( 'time' ); ?></div>
 <?php } ?>
 </div>
 
