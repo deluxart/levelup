29a64e6e1774714876f5555fba2b1856a32c7122
delux <deluxart3@gmail.com>
2019-10-02 11:30:38 +0300

    fix

--- LevelUp/functions.php
+++ LevelUp/functions.php
@@ -888,7 +888,7 @@ add_shortcode('events-mini', 'my_shortcode_function');
 function my_shortcode_function() {
 	global $wp_query;
 	$wp_query = new WP_Query(array(
-		'category' => '141',
+		'category' => '32',
 		'post_type' => 'post',
 		'posts_per_page' => '4',
 		// 'paged' => get_query_var('paged') ?: 1
