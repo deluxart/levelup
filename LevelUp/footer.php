415255e4d7b1e15398f1c2c131b73acc5daf3db4
delux <deluxart3@gmail.com>
2019-10-03 16:03:55 +0300

    fix

--- LevelUp/footer.php
+++ LevelUp/footer.php
@@ -105,17 +105,20 @@
     <div class="modalClose"></div>
     <div class="subscribe-modal-inner">
             <div class="subscribe-content">
-                <div class="widget first"><?php echo do_shortcode('[recent_posts num="3" cat="2874"]'); ?></div>
-                <div class="form">
-                    <h2>Будь вкурсе!</h2>
-                    <p>Актуальные новости и мероприятия в
-    еженедельной рассылке от Level Up</p>
+                <div class="top">
+                    <div class="widget first"><?php echo do_shortcode('[recent_posts num="3" cat="2874"]'); ?></div>
+                    <div class="form">
+                        <h2>Будь вкурсе!</h2>
+                        <p>Актуальные новости и мероприятия в
+        еженедельной рассылке от Level Up</p>
 
-                    <div class="form-cont">
-                    <?php echo do_shortcode('[contact-form-7 id="294" title="Футер (Форма подписки)"]'); ?>
+                        <div class="form-cont">
+                        <?php echo do_shortcode('[contact-form-7 id="294" title="Футер (Форма подписки)"]'); ?>
+                        </div>
                     </div>
+                    <div class="widget last"><?php echo do_shortcode('[recent_posts num="3" cat="2205"]'); ?></div>
                 </div>
-                <div class="widget last"><?php echo do_shortcode('[recent_posts num="3" cat="2205"]'); ?></div>
+                <div class="bottom">* Вы так-же в любой момент с легкостью сможете отписаться от нашей рассылки</div>
             </div>
     </div>
 </div>
