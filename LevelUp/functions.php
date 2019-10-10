7e6a2d0ccc4b331a9f99dd8408f5eeeff506e171
delux <deluxart3@gmail.com>
2019-10-10 12:54:22 +0300

    fix

--- LevelUp/functions.php
+++ LevelUp/functions.php
@@ -686,3 +686,24 @@ function shapeSpace_recent_posts_shortcode($atts, $content = null) {
 
 }
 add_shortcode('recent_posts', 'shapeSpace_recent_posts_shortcode');
+
+
+
+
+
+
+
+
+function simple_spoiler_shortcode($atts, $content) {
+	if ( ! isset($atts['title']) ) {
+		$sp_name = __( 'Spoiler', 'simple-spoiler' );
+	} else {
+		$sp_name = $atts['title'];
+	}
+	return '<div class="spoiler">
+				<div class="head">'.$sp_name.'</div>
+				<div class="cont">'.$content.'</div>
+			</div>';
+}
+add_shortcode( 'spoiler', 'simple_spoiler_shortcode' );
+add_filter( 'comment_text', 'do_shortcode' );
