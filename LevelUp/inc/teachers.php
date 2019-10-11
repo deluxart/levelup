afa24769e87ba2b9d2a65e57f55dd46789dd2fc4
delux <deluxart3@gmail.com>
2019-10-11 15:57:45 +0300

    fix

--- LevelUp/inc/teachers.php
+++ LevelUp/inc/teachers.php
@@ -83,4 +83,79 @@ add_shortcode( 'teacher',  'call_shortcode_teacher' );
         return $out;
     }
 
+
+
+
+
+
+
+
+    dd_action( 'manage_teachers_posts_custom_column', 'my_manage_teachers_columns', 10, 2 );
+
+    function my_manage_teachers_columns( $column, $post_id ) {
+        global $post;
+
+        switch( $column ) {
+
+            /* If displaying the 'scode' column. */
+            case 'scode' :
+
+                /* Get the post meta. */
+                $scode = get_post_meta( $post_id, 'scode', true );
+
+                /* If no scode is found, output a default message. */
+                if ( empty( $scode ) )
+                    echo __( 'Unknown' );
+
+                /* If there is a scode, append 'minutes' to the text string. */
+                else
+                    printf( __( '%s minutes' ), $scode );
+
+                break;
+
+            /* If displaying the 'genre' column. */
+            case 'genre' :
+
+                /* Get the genres for the post. */
+                $terms = get_the_terms( $post_id, 'genre' );
+
+                /* If terms were found. */
+                if ( !empty( $terms ) ) {
+
+                    $out = array();
+
+                    /* Loop through each term, linking to the 'edit posts' page for the specific term. */
+                    foreach ( $terms as $term ) {
+                        $out[] = sprintf( '&lt;a href="%s">%s&lt;/a>',
+                            esc_url( add_query_arg( array( 'post_type' => $post->post_type, 'genre' => $term->slug ), 'edit.php' ) ),
+                            esc_html( sanitize_term_field( 'name', $term->name, $term->term_id, 'genre', 'display' ) )
+                        );
+                    }
+
+                    /* Join the terms, separating them with a comma. */
+                    echo join( ', ', $out );
+                }
+
+                /* If no terms were found, output a default message. */
+                else {
+                    _e( 'No Genres' );
+                }
+
+                break;
+
+            /* Just break out of the switch statement for everything else. */
+            default :
+                break;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
 ?>
