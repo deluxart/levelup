3c3394aedee556a11e78b5f3437621860e54cd6b
delux <deluxart3@gmail.com>
2019-10-11 16:40:45 +0300

    fix

--- LevelUp/inc/teachers.php
+++ LevelUp/inc/teachers.php
@@ -2,31 +2,31 @@
 add_action( 'init', 'health_products' ); // Использовать функцию только внутри хука init
 
 function health_products() {
-	$labels = array(
-		'name' => 'Преподаватели',
-		'singular_name' => 'Препод', // админ панель Добавить->Функцию
-		'add_new' => 'Добавить препода',
-		'add_new_item' => 'Добавить нового преподавателя', // заголовок тега <title>
-		'edit_item' => 'Редактировать преподавателя',
-		'new_item' => 'Новый преподаватель',
-		'all_items' => 'Все преподаватели',
-		// 'view_item' => 'Просмотр продукта на сайте',
-		'search_items' => 'Искать преподавателя',
-		'not_found' =>  'Преводавателей не найдено.',
-		'not_found_in_trash' => 'В корзине нет преподавателей.',
-		'menu_name' => 'Преподаватели' // ссылка в меню в админке
-	);
-	$args = array(
-		'labels' => $labels,
-		'public' => false,
+    $labels = array(
+        'name' => 'Преподаватели',
+        'singular_name' => 'Препод', // админ панель Добавить->Функцию
+        'add_new' => 'Добавить препода',
+        'add_new_item' => 'Добавить нового преподавателя', // заголовок тега <title>
+        'edit_item' => 'Редактировать преподавателя',
+        'new_item' => 'Новый преподаватель',
+        'all_items' => 'Все преподаватели',
+        // 'view_item' => 'Просмотр продукта на сайте',
+        'search_items' => 'Искать преподавателя',
+        'not_found' =>  'Преводавателей не найдено.',
+        'not_found_in_trash' => 'В корзине нет преподавателей.',
+        'menu_name' => 'Преподаватели' // ссылка в меню в админке
+    );
+    $args = array(
+        'labels' => $labels,
+        'public' => false,
         //'rewrite' => array('slug' => 'health/%health%'),
-		'show_ui' => true, // показывать интерфейс в админке
-		'has_archive' => true,
-		'menu_icon' => 'dashicons-businessman', // иконка корзины
-		'menu_position' => 20, // порядок в меню
-		'supports' => array( 'title', 'editor', 'comments', 'author', 'thumbnail', 'revisions')
-	);
-	register_post_type('teachers', $args);
+        'show_ui' => true, // показывать интерфейс в админке
+        'has_archive' => true,
+        'menu_icon' => 'dashicons-businessman', // иконка корзины
+        'menu_position' => 20, // порядок в меню
+        'supports' => array( 'title', 'editor', 'comments', 'author', 'thumbnail', 'revisions')
+    );
+    register_post_type('teachers', $args);
 }
 
 
@@ -49,10 +49,10 @@ add_action("admin_init", "admin_init");
 <?php
     }
 
-	function save_job_position(){
-		global $post;
-		update_post_meta($post->ID, "job_position", $_POST["job_position"]);
-	}
+    function save_job_position(){
+        global $post;
+        update_post_meta($post->ID, "job_position", $_POST["job_position"]);
+    }
 
 
 
@@ -86,74 +86,75 @@ add_shortcode( 'teacher',  'call_shortcode_teacher' );
 
 
 
+add_filter( 'manage_edit-teachers_columns', 'my_edit_movie_columns' ) ;
+
+function my_edit_movie_columns( $columns ) {
+
+    $columns = array(
+        'cb' => '&lt;input type="checkbox" />',
+        'riv_post_thumbs' => __('Фото'),
+        'title' => __( 'Имя преподавателя' ),
+        'shortcode' => __( 'Шорткод' ),
+        'date' => __( 'Date' )
+    );
+
+    return $columns;
+}
 
 
 
 
-    dd_action( 'manage_teacher_posts_custom_column', 'my_manage_teacher_columns', 10, 2 );
 
-    function my_manage_teacher_columns( $column, $post_id ) {
-        global $post;
+add_action( 'manage_teachers_posts_custom_column', 'my_manage_teachers_columns', 10, 2 );
 
-        switch( $column ) {
+function my_manage_teachers_columns( $column, $post_id ) {
+    global $post;
 
-            /* If displaying the 'scode' column. */
-            case 'scode' :
+    switch( $column ) {
 
-                /* Get the post meta. */
-                $scode = get_post_meta( $post_id, 'scode', true );
+        /* If displaying the 'duration' column. */
+        case 'shortcode' :
 
-                /* If no scode is found, output a default message. */
-                if ( empty( $scode ) )
-                    echo __( 'Unknown' );
+            /* Get the post meta. */
+            // $duration = get_post_meta( $post_id, 'job_position', true );
+            $shortcode = $post->ID;
 
-                /* If there is a scode, append 'minutes' to the text string. */
-                else
-                    printf( __( '%s minutes' ), $scode );
+            /* If no duration is found, output a default message. */
+            if ( empty( $shortcode ) )
+                echo __( 'Unknown' );
 
-                break;
+            /* If there is a duration, append 'minutes' to the text string. */
+            else
+                printf( __( '[teacher id="%s"]' ), $shortcode );
 
-            /* If displaying the 'genre' column. */
-            case 'genre' :
+            break;
 
-                /* Get the genres for the post. */
-                $terms = get_the_terms( $post_id, 'genre' );
 
-                /* If terms were found. */
-                if ( !empty( $terms ) ) {
 
-                    $out = array();
+        /* If displaying the 'duration' column. */
+        case 'riv_post_thumbs' :
 
-                    /* Loop through each term, linking to the 'edit posts' page for the specific term. */
-                    foreach ( $terms as $term ) {
-                        $out[] = sprintf( '&lt;a href="%s">%s&lt;/a>',
-                            esc_url( add_query_arg( array( 'post_type' => $post->post_type, 'genre' => $term->slug ), 'edit.php' ) ),
-                            esc_html( sanitize_term_field( 'name', $term->name, $term->term_id, 'genre', 'display' ) )
-                        );
-                    }
+            /* Get the post meta. */
+            $photo = the_post_thumbnail( 'featured-thumbnail' );
 
-                    /* Join the terms, separating them with a comma. */
-                    echo join( ', ', $out );
-                }
+            /* If no duration is found, output a default message. */
+            if ( empty( $photo ) );
 
-                /* If no terms were found, output a default message. */
-                else {
-                    _e( 'No Genres' );
-                }
+            /* If there is a duration, append 'minutes' to the text string. */
+            else
+                printf( $photo );
 
-                break;
+            break;
 
-            /* Just break out of the switch statement for everything else. */
-            default :
-                break;
-        }
+
+
+
+
+        /* Just break out of the switch statement for everything else. */
+        default :
+            break;
     }
-
-
-
-
-
-
+}
 
 
 
