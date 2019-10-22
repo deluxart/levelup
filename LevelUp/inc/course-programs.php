ff2ee5962506b03c2adf1f97553a201489db2d85
delux <deluxart3@gmail.com>
2019-10-22 13:45:49 +0300

    fix

--- LevelUp/inc/course-programs.php
+++ LevelUp/inc/course-programs.php
@@ -110,7 +110,7 @@ add_shortcode( 'program',  'call_shortcode_program' );
                 else
                     // printf( __( '[program id="%s"]' ), $shortcode );
                     // <input type="text" onfocus="this.select();" readonly="" value="[sg_popup id=6796]" class="large-text code">
-                    printf( __( '<input type="text" onfocus="this.select();" readonly="" value="[program id=%s]" class="large-text code">' ), $shortcode );
+                    printf( __( '<input type="text" onfocus="this.select();" style="width: auto" readonly="" value="[program id=%s]" class="large-text code">' ), $shortcode );
             break;
 
             default :
