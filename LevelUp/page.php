5b1904b43d72d75e7ef0bce9d2f7bb4be2a0b7d9
delux <deluxart3@gmail.com>
2019-10-25 11:09:07 +0300

    fix

--- LevelUp/page.php
+++ LevelUp/page.php
@@ -14,7 +14,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main <?php the_field( 'header_color' ); ?>" role="main">
 
 		<?php
 		// Start the loop.
