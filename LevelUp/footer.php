a33b23d7267529048c91b1fb5dadd18ca3a4d3c3
delux <deluxart3@gmail.com>
2019-10-31 16:55:22 +0200

    fix

--- LevelUp/footer.php
+++ LevelUp/footer.php
@@ -190,9 +190,9 @@ jQuery(document).ready(function(){
 
         <?php wp_footer(); ?>
         <script>
-        jQuery(".course_card").flip(
+        jQuery(".course_card").flip({
             trigger: 'hover'
-        );
+        });
         </script>
         <?php echo $options['jivosite_code'];?>
         <?php echo $options['binotel_code'];?>
