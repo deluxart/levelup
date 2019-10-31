717afe2a2640591a7842db96c1fa08052d7b3896
delux <deluxart3@gmail.com>
2019-10-31 09:23:53 +0200

    fix

--- LevelUp/js/scripts.js
+++ LevelUp/js/scripts.js
@@ -685,17 +685,15 @@ jQuery('#subscribe-modal .icon--close').click(function () {
 
 
 jQuery(document).ready(function() {
-    var img_active = jQuery('#home_slider .home-head-slider .slick-current').children('img').attr('src');
-
-
     // jQuery('#home_slider > .stub-header > img').text(jQuery('.slick-active #inputText').text());
     // jQuery('#home_slider > .stub-header > img').css({ background: "url(' + img_active + ')" });
     jQuery('#home_slider > .stub-header > img').css({'background-image': 'url(' + img_active + ')'});
 
     jQuery('.home-head-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    var img_active2 = jQuery('#home_slider .home-head-slider .slick-current').children('img').attr('src');
     jQuery('#home_slider > .stub-header > img').fadeOut().css({'background-image': 'none'});
         setTimeout(function(){
-            jQuery('#home_slider > .stub-header > img').fadeIn().css({'background-image': 'url(' + img_active + ')'});
+            jQuery('#home_slider > .stub-header > img').fadeIn().css({'background-image': 'url(' + img_active2 + ')'});
         },100);
     });
 });
