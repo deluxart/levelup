cb6c5b9ee11c66feb77b21d85a4a96d8d70fa78b
delux <deluxart3@gmail.com>
2019-10-31 13:25:22 +0200

    fix

--- LevelUp/page-question.php
+++ LevelUp/page-question.php
@@ -43,6 +43,13 @@ jQuery(".c100").each(function() {
     jQuery(this).html(text);
 });
 </script>
+
+
+
+
+
+
+
 <?php
 /**
  * The template for displaying the footer
@@ -54,62 +61,19 @@ jQuery(".c100").each(function() {
  * @since LevelUp 1.0
  */
 ?>
-
 	</div><!-- .site-content -->
 
-	<footer id="footer" class="site-footer">
 
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12 col-md-6 col-lg-4">
-
-                    <?php
-                    if ( function_exists('dynamic_sidebar') )
-                        dynamic_sidebar('footer-1');
-                    ?>
-
-            </div>
-            <div class="col-sm-12 col-md-6 col-lg-4 col-footer-2">
-
-                    <?php
-                    if ( function_exists('dynamic_sidebar') )
-                        dynamic_sidebar('footer-2');
-                    ?>
-
-            </div>
-            <div class="col-sm-12 col-md-6 col-lg-4">
-
-                    <?php
-                    if ( function_exists('dynamic_sidebar') )
-                        dynamic_sidebar('footer-3');
-                    ?>
-
-            </div>
-        </div>
-    </div>
-	</footer><!-- .site-footer -->
 
 </div><!-- .site -->
 
-<?php wp_footer(); ?>
 
-<script type="text/javascript"> jQuery(document).ready(function () {
-   if(window.location.href.indexOf("#open-reg") > -1) {
-    jQuery("#open-reg .sg-show-popup").click();
-  }
-}); </script>
-<!-- Start обратный звонок binotel -->
 
-<!-- End обратный звонок binotel -->
-<script scr="<?php echo get_template_directory_uri(); ?>/video-js/videojs-background.js"></script>
-<script scr="<?php echo get_template_directory_uri(); ?>/video.min.js"></script>
-		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/scripts.js"></script>
-		<script src="<?php echo get_template_directory_uri(); ?>/js/imagesloaded.pkgd.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.js"></script>
-		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/slick/slick.min.js"></script>
-		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/slick_slides.js"></script>
-<!-- Мероприятия -->
-<!--Конец Мероприятия -->
+
+
+
+
+        <?php wp_footer(); ?>
+
 </body>
 </html>
-
