22745c73177ba3fdf780e14e015a92d8891688e2
delux <deluxart3@gmail.com>
2019-11-01 10:39:51 +0200

    fix

--- LevelUp/js/scripts.js
+++ LevelUp/js/scripts.js
@@ -680,30 +680,37 @@ jQuery('#subscribe-modal .icon--close').click(function () {
 
 
 
-// $("#image").stop().animate({opacity: 0},1000,function(){
-//     $(this).css({'background-image': "url('/images/alt_image.png')"})
-//                .animate({opacity: 1},{duration:1000});
-//  });
 
 
-
-
+// For new home page
 
 jQuery(document).ready(function() {
     var img_active = jQuery('#home_slider .home-head-slider .slick-current').children('img').attr('src');
-    // jQuery('#home_slider > .stub-header > img').text(jQuery('.slick-active #inputText').text());
-    // jQuery('#home_slider > .stub-header > img').css({ background: "url(' + img_active + ')" });
     jQuery('#home_slider > .stub-header > .stub-header-bg').css({'background-image': 'url(' + img_active + ')'});
 
     jQuery('.home-head-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
-    // jQuery('#home_slider > .stub-header > img').css({'background-image': 'none'});
         setTimeout(function(){
             var img_active2 = jQuery('#home_slider .home-head-slider .slick-current').children('img').attr('src');
-
-            // jQuery('#home_slider > .stub-header > .stub-header-bg').fadeIn().css({'background-image': 'url(' + img_active2 + ')'}).animate({opacity: 1},{duration:1000});
             jQuery('#home_slider > .stub-header > .stub-header-bg').stop().animate({opacity: 0},300,function(){
                 jQuery(this).css({'background-image': 'url(' + img_active2 + ')'}).animate({opacity: 1},{duration:300});
              });
         },100);
     });
+});
+
+
+
+jQuery(document).ready(function(){
+
+    jQuery('ul.un-tabs li').click(function(){
+        var tab_id = jQuery(this).attr('data-tab');
+
+        jQuery('ul.un-tabs > li').removeClass('current');
+        // jQuery('.un-content').removeClass('current');
+        jQuery('.un-content.current').fadeOut(100).removeClass('current');
+
+        jQuery(this).addClass('current');
+        jQuery("#"+tab_id).fadeIn(200).addClass('current');
+    });
+
 });
