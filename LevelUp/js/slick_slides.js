411970aae74946378f7c10fe66ea320083c22327
delux <deluxart3@gmail.com>
2019-11-04 15:39:28 +0200

    fix

--- LevelUp/js/slick_slides.js
+++ LevelUp/js/slick_slides.js
@@ -282,3 +282,38 @@ jQuery('.home-head-slider').slick({
             }
           ]
         });
+
+
+        jQuery('.news_slider').slick({
+          dots: true,
+          adaptiveHeight: false,
+          infinite: true,
+          speed: 300,
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 769,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        });
