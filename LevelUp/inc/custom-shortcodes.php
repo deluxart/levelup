fceed8150ab3cdd11c96b4921b1405a400e661ca
delux <deluxart3@gmail.com>
2019-11-05 10:43:55 +0200

    fix

--- LevelUp/inc/custom-shortcodes.php
+++ LevelUp/inc/custom-shortcodes.php
@@ -30,13 +30,16 @@ function rmcc_post_listing_parameters_shortcode( $atts ) {
     $query = new WP_Query( $options );
     // run the loop based on the query
     if ( $query->have_posts() ) { ?>
-
-<?php
-                get_template_part( 'template-parts/lastnews', get_post_format() );
-?>
-    <?php
-        $myvariable = ob_get_clean();
-        return $myvariable;
+        <ul class="clothes-listing">
+            <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+            <li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </li>
+            <?php endwhile;
+            wp_reset_postdata(); ?>
+        </ul>
+    <?php $myvariable = ob_get_clean();
+    return $myvariable;
     }
 }
 
