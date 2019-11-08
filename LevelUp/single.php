9e49124f3cfc65d5e9581cbbf503dd79b16565a5
delux <deluxart3@gmail.com>
2019-11-08 14:41:24 +0200

    fix

--- LevelUp/single.php
+++ LevelUp/single.php
@@ -8,35 +8,81 @@
  */
 
 get_header(); ?>
-<main id="main" class="site-main" role="main">
-<div class="container">
-	<div class="row" id="content-page">
 
-	<div id="content-news" class="course content-area">
+<main id="main" class="site-main full_post" role="main">
+<div class="container">
+	<div id="content-page">
+
+
+
+	<div id="content-news" class="course content-area 222">
 
 		<?php
 		// Start the loop.
 		while ( have_posts() ) : the_post();
 			get_template_part( 'content', get_post_format() );
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-			the_post_navigation( array(
-				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'LevelUp' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Next post:', 'LevelUp' ) . '</span> ' .
-					'<span class="post-title">%title</span>',
-				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'LevelUp' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Previous post:', 'LevelUp' ) . '</span> ' .
-					'<span class="post-title">%title</span>',
-			) );
 		endwhile;
 		?>
-
 		<!-- .site-main -->
 	</div><!-- .content-area -->
 
+
+
 </div>
 </div>
+
+
+
+
+		<?php $tags = wp_get_post_tags($post->ID);
+		if ($tags) {
+		$tag_ids = array();
+		foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
+		$args=array(
+			'tag__in' => $tag_ids,
+			'orderby' => rand,
+			'caller_get_posts' => 1,
+			'post__not_in' => array($post->ID),
+			'showposts' => 3
+		);
+		$my_query = new wp_query($args);
+		if( $my_query->have_posts() ) {
+		echo '<div class="related_posts">';
+		echo '<div class="container">';
+		echo '<h2 class="widget-title">Похожие записи:</h3>';
+		echo '<ul>';
+		while ($my_query->have_posts()) {
+		$my_query->the_post();
+		?>
+		<li>
+			<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>">
+				<div class="post-thumb"><?php the_post_thumbnail(); ?></div>
+				<div>
+					<?php the_title(); ?>
+					<span class="date_publ"><?php the_date(); ?></span>
+				</div>
+			</a>
+		</li>
+		<?php
+		}
+		echo '</ul>';
+		echo '</div>';
+		echo '</div>';
+		}
+		wp_reset_query();
+		}
+		?>
+
+
+<div class="news_sidebar">
+	<div class="container">
+		<?php
+			if ( function_exists('dynamic_sidebar') )
+				dynamic_sidebar('after-news-sidebar');
+		?>
+	</div>
+</div>
+
+
 </main>
 <?php get_footer(); ?>
