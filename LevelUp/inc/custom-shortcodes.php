0b3a2819f367ffa241147325ac848c2095e6d5a3
delux <deluxart3@gmail.com>
2019-11-11 11:17:36 +0200

    add posts shortcode

--- LevelUp/inc/custom-shortcodes.php
+++ LevelUp/inc/custom-shortcodes.php
@@ -10,7 +10,7 @@ function rmcc_post_listing_parameters_shortcode( $atts ) {
         'posts' => -1,
         'color' => '',
         'category' => '',
-        'post_status' => 'publish',
+        // 'post_status' => 'publish',
         'public'   => true,
     ), $atts );
     $options = array(
@@ -19,7 +19,8 @@ function rmcc_post_listing_parameters_shortcode( $atts ) {
         'orderby' => $args['orderby'],
         'posts_per_page' => $args['posts'],
         'color' => $args['color'],
-        'category_name' => $args['category']
+        'category_name' => $args['category'],
+        'post_status' => 'publish'
     );
 
     $query = new WP_Query( $options );
@@ -34,4 +35,83 @@ function rmcc_post_listing_parameters_shortcode( $atts ) {
     return $myvariable;
     }
 }
+
+
+
+add_shortcode( 'posts', 'rmcc_post_cats_parameters_shortcode' );
+function rmcc_post_cats_parameters_shortcode( $atts ) {
+    ob_start();
+    $args = shortcode_atts( array (
+        'type' => 'post',
+        'order' => 'date',
+        'orderby' => 'title',
+        'posts' => -1,
+        'color' => '',
+        'category' => '',
+        'public'   => true,
+    ), $atts );
+    $options = array(
+        'post_type' => $args['type'],
+        'order' => $args['order'],
+        'orderby' => $args['orderby'],
+        'posts_per_page' => $args['posts'],
+        'color' => $args['color'],
+        'category_name' => $args['category'],
+        'paged' => get_query_var('paged') ?: 1,
+        'post_status' => 'publish'
+    );
+
+    $query = new WP_Query( $options );
+    if ( $query->have_posts() ) { ?>
+            <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+                <?php
+                    get_template_part( 'template-parts/posts', get_post_format() );
+                 ?>
+            <?php endwhile;
+            posts_nav_link();
+            wp_reset_postdata(); ?>
+    <?php $myvariable = ob_get_clean();
+    return $myvariable;
+    }
+}
+
+
+
+
+
+
+
+// add_shortcode('posts', 'my_shortcode_function');
+// function my_shortcode_function() {
+// 	global $wp_query;
+// 	$wp_query = new WP_Query(array(
+// 		// 'category_name' => 'portfolio',
+// 		'post_type' => 'post',
+// 		'posts_per_page' => '6',
+// 		'paged' => get_query_var('paged') ?: 1
+// 	));
+// ob_start();
+// echo '<div class="portfolio">';
+// 	if ( have_posts() ) :
+// 	        while ( have_posts() ) : the_post();
+
+// 	            get_template_part( 'template-parts/posts', get_post_format() );
+
+// 	        endwhile;
+// 	    else :
+// 	        get_template_part( 'template-parts/content', 'none' );
+// 	    endif;
+// echo '</div>';
+
+// 	posts_nav_link();
+// 	wp_reset_query();
+// 	$out = ob_get_clean();
+// 	return $out;
+// }
+
+
+
+
+
+
 ?>
