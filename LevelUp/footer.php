1ecb85f7f85e8eaa79a0366157a755667cd5407f
delux <deluxart3@gmail.com>
2019-11-14 14:53:23 +0200

    fix

--- LevelUp/footer.php
+++ LevelUp/footer.php
@@ -253,7 +253,7 @@ document.querySelectorAll('section[data-bg]')
 <script type="text/javascript">
 	jQuery(document).ready(function() {
 		jQuery(".eTimer").eTimer({
-			etType: 0, etDate: "02.11.2019.10.0", etTitleText: "<a href='https://levelup.ua/it-conference-2019'>До початку:</a>", etTitleSize: 16, etShowSign: 3, etSep: ":", etFontFamily: "Trebuchet MS", etTextColor: "#a3a3a3", etPaddingTB: 15, etPaddingLR: 15, etBackground: "#333333", etBorderSize: 0, etBorderRadius: 2, etBorderColor: "white", etShadow: " 0px 0px 0px 0px #333333", etLastUnit: 4, etNumberFontFamily: "Impact", etNumberSize: 24, etNumberColor: "black", etNumberPaddingTB: 0, etNumberPaddingLR: 8, etNumberBackground: "#eaff00", etNumberBorderSize: 0, etNumberBorderRadius: 5, etNumberBorderColor: "white", etNumberShadow: "inset 0px 0px 10px 0px rgba(0, 0, 0, 0.5)"
+			etType: 0, etDate: "02.11.2019.10.0", etTitleText: "<a href='https://levelup.ua/it-conference-2019'>До початку:</a>", etTitleSize: 16, etShowSign: 3, etSep: ":", etFontFamily: "Trebuchet MS", etTextColor: "#a3a3a3", etPaddingTB: 15, etPaddingLR: 15, etBackground: "#333333", etBorderSize: 0, etBorderRadius: 2, etBorderColor: "white", etShadow: " 0px 0px 0px 0px #333333", etLastUnit: 4, etNumberFontFamily: "Impact", etNumberSize: 24, etNumberColor: "black", etNumberPaddingTB: 0, etNumberPaddingLR: 8, etNumberBackground: "#ffde00", etNumberBorderSize: 0, etNumberBorderRadius: 5, etNumberBorderColor: "white", etNumberShadow: "inset 0px 0px 10px 0px rgba(0, 0, 0, 0.5)"
 		});
 	});
 </script> -->
