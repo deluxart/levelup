e5fc6e6eac8b6a2518e18489c38f1dc5e482be2f
delux <deluxart3@gmail.com>
2019-11-18 16:14:47 +0200

    fix

--- LevelUp/archive.php
+++ LevelUp/archive.php
@@ -85,9 +85,9 @@ get_header(); ?>
                 <?php
                             // Previous/next page navigation.
                             the_posts_pagination( array(
-                                'prev_text'          => pll_e('Previous page','LevelUp'),
-                                'next_text'          => pll_e('Next page','LevelUp'),
-                                'before_page_number' => '<span class="meta-nav screen-reader-text">' . pll_e('Page','LevelUp') . ' </span>',
+                                'prev_text'          => pll__('Previous page','LevelUp'),
+                                'next_text'          => pll__('Next page','LevelUp'),
+                                'before_page_number' => '<span class="meta-nav screen-reader-text">' . pll__('Page','LevelUp') . ' </span>',
                             ) );
                 ?>
                 </div>
