231ebcd7c1f285b143b577322c40a994f64501a6
delux <deluxart3@gmail.com>
2019-11-18 16:34:01 +0200

    fix

--- LevelUp/template-parts/archive.php
+++ LevelUp/template-parts/archive.php
@@ -8,17 +8,20 @@
  */
 
 ?>
-<div>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <div class="content-one">
         <div class="thumbnail">
 			<a href="<?php the_permalink(); ?>"><?php LevelUp_post_thumbnail(); ?></a>
             <div class="item-overlay"></div>
         </div>
-        <div class="title-news">
-        	<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-        </div>
 
+		<div class="details-news">
+            <span class="cat"><?php
+            $category = get_the_category();
+            echo $category[0]->cat_name;
+            ?></span>
+            <span class="spectator">/</span>
+			<span class="date"><?php echo get_the_date(); ?></span>
+		</div>
     </div>
 </article>
-</div>
