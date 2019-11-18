55ae8364985c377f1f6bfa063984ed2ef782c618
delux <deluxart3@gmail.com>
2019-11-18 15:54:03 +0200

    fix

--- LevelUp/template-parts/archive.php
+++ LevelUp/template-parts/archive.php
@@ -12,6 +12,7 @@
     <div class="content-one">
         <div class="thumbnail">
 			<a href="<?php the_permalink(); ?>"><?php LevelUp_post_thumbnail(); ?></a>
+            <div class="item-overlay"></div>
         </div>
         <div class="title-news">
         	<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
