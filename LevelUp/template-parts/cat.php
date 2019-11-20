d7636211c8e5934647f37d28408a9ebeef80261b
delux <deluxart3@gmail.com>
2019-11-20 09:54:40 +0200

    fix

---
+++ LevelUp/template-parts/cat.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?>
+<div id="lastpost-<?php the_ID(); ?>" class="catpost color-<?php $args['color'] ?>">
+    <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail(); ?></a>
+    <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+</div>
