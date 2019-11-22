f47af372834ee3676876a2f36ea196b33905c23f
delux <deluxart3@gmail.com>
2019-11-22 17:31:06 +0200

    fix

--- LevelUp/template-parts/open-courses.php
+++ LevelUp/template-parts/open-courses.php
@@ -21,7 +21,7 @@
 <?php } else { ?>
     <div class="course-block" id="course-<?php the_ID(); ?>">
 		<div class="m_head"><div class="icon"><img src="<?php the_field( 'logo_url' ); ?>" /></div>
-		<div class="name"><h4><?php the_title(); ?></h4><p style="font-style: italic;">Сортировка: <?php the_field( 'sort_courses' ); ?></p></div></div>
+		<div class="name"><h4><?php the_title(); ?></h4><p style="font-style: italic;"><?php the_field( 'description' ); ?></p></div></div>
 		<div class="date">
 			<span class="title">Старт:</span>
 			<h5><?php the_field( 'data_starta' ); ?></h5>
@@ -32,7 +32,7 @@
 			<p><?php the_field( 'price_before' ); ?></p>
 			<h5><?php the_field( 'stoimost' ); ?><span><?php the_field( 'znachenie_czeny' ); ?></span></h5>
 		</div>
-		<a href="/kurs-razrabotki-na-c-i-microsoft-net/" class="more-link">
+		<a href="<?php the_field( 'vybrat_kurs' ); ?>" class="more-link">
 			<span>Подробнее</span>
 		</a>
 </div>
