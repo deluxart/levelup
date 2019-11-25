ecadfbd87fbc2ee9ba3466874ba53fd2fd3b961f
delux <deluxart3@gmail.com>
2019-11-25 16:35:58 +0200

    fix

--- LevelUp/template-parts/program.php
+++ LevelUp/template-parts/program.php
@@ -14,3 +14,36 @@
         <?php the_content(); ?>
     </div>
 </div>
+
+
+<div id="program-<?php the_ID(); ?>">
+    <div class="program-block">
+
+
+
+
+
+
+    <?php if ( have_rows( 'programma_kursa' ) ) : ?>
+        <?php while ( have_rows( 'programma_kursa' ) ) : the_row(); ?>
+    <div class="spoiler parrent">
+	        <div class="head"><?php the_sub_field( 'title' ); ?></div>
+            <div class="cont">
+            <?php if ( have_rows( 'opisanie' ) ) : ?>
+                <ul>
+                <?php while ( have_rows( 'opisanie' ) ) : the_row(); ?>
+                    <li><?php the_sub_field( 'punkty' ); ?></li>
+                <?php endwhile; ?>
+                </ul>
+            <?php else : ?>
+                <?php // no rows found ?>
+            <?php endif; ?>
+            </div>
+        <?php endwhile; ?>
+    <?php else : ?>
+        <?php // no rows found ?>
+        </div>
+    <?php endif; ?>
+
+    </div>
+</div>
