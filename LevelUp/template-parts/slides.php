95307146600344619c9f6bd7526f1a6b479c352e
delux <deluxart3@gmail.com>
2019-11-25 13:42:02 +0200

    fix

--- LevelUp/template-parts/slides.php
+++ LevelUp/template-parts/slides.php
@@ -18,7 +18,7 @@ $currentDate = date('d.m.Y');
 if($currentDate <= $eventDate && get_field( 'add_home_slide' ) == 1) { ?>
 
 <div id="lastpost-<?php the_ID(); ?>" class="lastpost color-<?php $args['color'] ?>">
-    <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail(); ?></a>
+    <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail( 'full' ); ?></a>
 </div>
 
 <?php } ?>
