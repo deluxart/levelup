4a9d8a68600d14ecd26747225e8c3a6e42dd39c5
delux <deluxart3@gmail.com>
2019-11-26 15:42:37 +0200

    fix

--- LevelUp/inc/course-programs.php
+++ LevelUp/inc/course-programs.php
@@ -149,14 +149,10 @@ add_shortcode( 'program',  'call_shortcode_program' );
 
         ob_start();
         echo '<div class="program">[program id=';
-            if ( have_posts() ) :
-                    while ( have_posts() ) : the_post();
+            if ( have_posts() ) : ?>
+<?php the_field( 'programma_kursa' ); ?>
+            <?php
 
-                        get_template_part( 'template-parts/program-acf', get_post_format() );
-
-                    endwhile;
-                else :
-                    get_template_part( 'template-parts/content', 'none' );
                 endif;
         echo ']</div>';
 
