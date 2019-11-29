6863fa339b133edd3ef27e329619da3e7ea2d503
delux <deluxart3@gmail.com>
2019-11-29 13:07:19 +0200

    fix

--- LevelUp/inc/course-shortcodes.php
+++ LevelUp/inc/course-shortcodes.php
@@ -79,11 +79,18 @@ function my_manage_levelup_courses_columns( $column, $post_id ) {
         break;
 
         case 'data_starta' :
-            $start = the_sub_field( "date_start", $post->ID );
-            if ( empty( $start ) )
-                echo __( 'Не указана' );
-            else
-                printf( $start );
+
+
+            if ( have_rows( 'data_raspisanie_grafik' ) ) :
+                while ( have_rows( 'data_raspisanie_grafik' ) ) : the_row();
+                    $start = the_sub_field( "date_start", $post->ID );
+                    if ( empty( $start ) )
+                    echo __( 'Не указана' );
+                else
+                    printf( $start );
+                endwhile;
+            endif;
+
         break;
 
         case 'stoimost' :
