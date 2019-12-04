d5675d60f29c2c76ec044dc1749db4e190c04e19
delux <deluxart3@gmail.com>
2019-12-04 10:36:46 +0200

    fix

--- LevelUp/js/scripts.js
+++ LevelUp/js/scripts.js
@@ -461,7 +461,7 @@ jQuery('.spoiler.child > .head').on('click', function(e){
   e.preventDefault();
 });
 
-jQuery('body .program-block').not( ".page-id-529" ).ready(function () {
+jQuery('body:not(.page-id-529) .program-block').ready(function () {
     var s_head = jQuery('.program-block').children('.spoiler').children('.head').hasClass('active');
     var s_cont = jQuery('.program-block').children('.spoiler').children('.cont').hasClass('active');
 
