7cc8705fa645fdf836a7a142996d683228f8f216
delux <deluxart3@gmail.com>
2019-12-09 16:53:46 +0200

    fix

--- LevelUp/header.php
+++ LevelUp/header.php
@@ -67,8 +67,8 @@ if (isset($GLOBALS["polylang"])) {
 		          <img src="https://levelup.ua/wp-content/uploads/2019/11/level_up-1.svg" class="logo_desktop" alt="LevelUp" />
 		          <img src="https://levelup.ua/wp-content/uploads/2019/11/logo_mobile.svg" class="logo_mobile" alt="LevelUp" />
 		          <img src="https://levelup.ua/wp-content/uploads/2019/11/level_up-1.svg" class="dev-logo" alt="LevelUp" />
-                  <img src="https://levelup.ua/wp-content/uploads/2019/12/color_logo.svg" class="d-none color-logo" alt="LevelUp" alt="" />>
-                  <img src="https://levelup.ua/wp-content/uploads/2019/12/color_logo_mobile.svg" class="d-none color-logo-mobile" alt="LevelUp" alt="" />>
+                  <img src="https://levelup.ua/wp-content/uploads/2019/12/color_logo.svg" class="d-none color-logo" alt="LevelUp" alt="" />
+                  <img src="https://levelup.ua/wp-content/uploads/2019/12/color_logo_mobile.svg" class="d-none color-logo-mobile" alt="LevelUp" alt="" />
 		    </a>
 		</div>
         <div class="header-phone">
